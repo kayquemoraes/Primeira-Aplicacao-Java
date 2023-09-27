fe2e611e998eb5d9a23bb810feb5829e3ea55fd0
Kayque Moraes <kaymoraesdias@gmail.com>
2023-09-27 17:37:07 -0300

    commit initial

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 8.0 + 6.3) / 3;
+        System.out.println("Média: " + media);
+
+        String sinopse;
+        sinopse = "Filme de aventura com galã dos anos 80";
+        System.out.println(sinopse);
+    }
+}
